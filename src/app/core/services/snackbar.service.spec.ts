9d0d5ef1a769abe2e9b572e63462db276c6cb9d8
Gaspar Rodriguez Valero <Gaspar1992@users.noreply.github.com>
2020-05-25 11:51:43 +0200

    Added Prettier

--- src/app/core/services/snackbar.service.spec.ts
+++ src/app/core/services/snackbar.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { SnackbarService } from './snackbar.service';
+import { SnackbarService } from './snackbar.service'
 
 describe('SnackbarService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({}))
 
   it('should be created', () => {
-    const service: SnackbarService = TestBed.get(SnackbarService);
-    expect(service).toBeTruthy();
-  });
-});
+    const service: SnackbarService = TestBed.get(SnackbarService)
+    expect(service).toBeTruthy()
+  })
+})
